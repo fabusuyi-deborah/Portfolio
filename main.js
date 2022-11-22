b3a7ffba792a573ec2eed68f6dbb55e8832939ca
Fabusuyideborah <fabusuyideborah03@gmail.com>
2022-11-22 15:51:40 +0100

    added some changes

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function toggleMobileMenu(menu) {
-  menu.classList.toggle('open');
-}
+const hamburger = document.querySelector('.hamburger');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+	hamburger_icon.innerText = hamburger_icon.innerText === 'menu' 
+		? 'close'
+		: 'menu';
+	
+	mobile_menu.classList.toggle('is-open');
+})
